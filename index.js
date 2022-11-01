7bb5d9541ed2edc71f8f258b661059ead4a32c52
tor <torfrancis447@gmail.com>
2022-11-01 12:16:06 -0400

    commit tree 9b985d8e39358e9a7602ad4c51672094aff7c8c1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John',
+    streetAddress: '15 L street'
+}
+function updateEmployeeWithKeyAndValue() {
+    const newEmployee = {...employee}
+    newEmployee.name = 'Sam';
+    newEmployee.streetAddress ='11 Broadway';
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway'
+    return employee
+}
+function deleteFromEmployeeByKey() {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+}
